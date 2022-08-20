38ce74b3e5b6bb944e8b01ee9f0ae44a707a6b63
Padiar <guga.padiar@gmail.com>
2022-08-20 20:30:18 -0300

    Estudando Java

---
+++ aula 02/Estacionamento/src/Carro.java
@@ -0,0 +1,27 @@
+public class Carro {
+    // atributos
+    String placa;
+    String marca;
+    String modelo;
+    String cor;
+    double valor;
+
+    // metodos
+    void acelerar(){
+
+    }
+
+    void estacionar(){
+
+    }
+
+    void mostrardados(){
+        System.out.println("Dados do Carro");
+        System.out.println("Placa: " + placa);
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Cor: " + cor);
+        System.out.println("Valor: " + valor);
+        System.out.println("=-=-=-==-=-=-=-=-=");
+    }
+}
