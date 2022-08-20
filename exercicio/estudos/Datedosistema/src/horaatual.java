979723d067042a575ee8a5ffe29bafd909ab45c8
Gustavo Effgen Padiar Pereira <79231839+Padiar@users.noreply.github.com>
2022-08-20 20:38:53 -0300

    Add files via upload

---
+++ exercicio/estudos/Datedosistema/src/horaatual.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class horaatual
+{
+    public static void main(String[] args) {
+        Date date = new Date();
+            System.out.println("A Hora atual é: ");
+            System.out.println(date.toString());
+    }
+}
